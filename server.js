98a0705ef2227dfab31f2c1455f19ef38646dd39
bank-github <6431501089@lamduan.mfu.ac.th>
2024-04-22 14:27:27 +0700

    aos8 mongodb version

---
+++ server.js
@@ -0,0 +1,130 @@
+var mysql = require('mysql');
+const WebSocket = require('ws');
+const protobuf = require('protobufjs');
+const aruba_telemetry_proto = require('./aruba_iot_proto.js').aruba_telemetry;
+var MongoClient = require('mongodb').MongoClient;
+
+
+const wss = new WebSocket.Server({ port: 3003 });
+// สร้าง websockets server ที่ port 4000
+wss.on('connection', function connection(ws) { // สร้าง connection
+  ws.on('message', function incoming(message) {
+
+    // รอรับ data อะไรก็ตาม ที่มาจาก client แบบตลอดเวลา
+    //console.log('received: %s', message);
+    let telemetryReport = aruba_telemetry_proto.Telemetry.decode(message);
+    //telemetryReport.toJSON();
+    let myobj = JSON.stringify(telemetryReport);
+    let obj = JSON.parse(myobj);
+    //console.log(myobj);
+
+    if (obj["reported"] == null) {
+      console.log("Aruba Websocket Established");
+    } else {
+      console.log(myobj);
+      console.log(obj["reporter"]["name"]);
+      //console.log(obj["reporter"]["ipv4"]);
+      console.log(obj["reported"]);
+      //console.log(obj.reported);
+      add_sensors(obj["reporter"]["name"], obj.reported);
+      //console.log(obj.reported[0]["deviceClass"]);
+      /*let count=0;
+      for (k of obj.reported) {
+        console.log(obj.reported[count]["deviceClass"]);
+        console.log("===================================");
+        if(obj.reported[count]["deviceClass"].includes('iBeacon')==true){
+          console.log(obj.reported[count]["beacons"][0]['ibeacon']['uuid']);
+          console.log('iBaecon');
+        }
+        if(obj.reported[count]["deviceClass"]=='arubaTag'){
+          console.log('ArubaTag');
+        }
+        count +=1;
+      }*/
+    }
+    //console.log(telemetryReport.body);
+  });
+  ws.on('close', function close() {
+    // จะทำงานเมื่อปิด Connection ในตัวอย่างคือ ปิด Browser
+    console.log('disconnected');
+  });
+  ws.send('init message to client');
+  // ส่ง data ไปที่ client เชื่อมกับ websocket server นี้
+});
+async function beacon(report) {
+  var c = 0;
+  for (k of report) {
+    c += 1;
+    console.log(c);
+    console.log(k.beacons);
+  }
+}
+
+function add_sensors(location, sensor) {
+  let count = 0;
+  for (k of sensor) {
+    console.log("===================================");
+    if (sensor[count]["deviceClass"].includes('iBeacon') == true && sensor[count]["deviceClass"].includes('arubaBeacon') == false && sensor[count]['rssi'] != null) {
+      let data = {
+        mac: sensor[count]['mac'],
+        deviceClass: 'iBeacon',
+        rssi: sensor[count]['rssi']['max'],
+        timeStamp: new Date().toISOString(),
+        major: sensor[count]["beacons"][0]['ibeacon']['major'],
+        minor: sensor[count]["beacons"][0]['ibeacon']['minor'],
+        location: location
+      };
+      add_db(data);
+
+    } else if (sensor[count]["deviceClass"] == 'eddystone' && sensor[count]['rssi'] != null) {
+      let data = {
+        mac: sensor[count]['mac'],
+        deviceClass: 'eddystone',
+        rssi: sensor[count]['rssi']['max'],
+        timeStamp: new Date().toISOString(),
+        location: location,
+        dynamicValue: sensor[count]['sensors']['temperatureC']
+      };
+      add_db(data);
+    } else if (sensor[count]["deviceClass"] == 'unclassified' && sensor[count]['rssi'] != null) {
+      let data = {
+        mac: sensor[count]['mac'],
+        deviceClass: 'unclassified',
+        rssi: sensor[count]['rssi']['max'],
+        timeStamp: new Date().toISOString(),
+        location: location,
+        dynamicValue: sensor[count]['stats']['frame_cnt']
+      };
+      add_db(data);
+    }
+    count += 1;
+  }
+}
+
+async function add_db(data) {
+  try {
+    const url = "mongodb://localhost:27017/";
+    const client = await MongoClient.connect(url);
+    const db = client.db("BLE");
+    console.log(data);
+    const result = await db.collection("signal").insertOne(data);
+    console.log("1 document inserted");
+    client.close();
+    return result;
+  } catch (err) {
+    console.error("Error inserting document:", err);
+    throw err;
+  }
+}
+
+/*
+console.log(obj.reported[count]["deviceClass"]);
+    console.log("===================================");
+    if(obj.reported[count]["deviceClass"].includes('iBeacon')==true){
+      console.log(obj.reported[count]["beacons"][0]['ibeacon']['uuid']);
+      console.log('iBaecon');
+    }
+    if(obj.reported[count]["deviceClass"]=='arubaTag'){
+      console.log('ArubaTag');
+    }
+*/
