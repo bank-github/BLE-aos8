5c3938fe828eb2bd2baa66c1f68ccd723c12bcbb
bank-github <6431501089@lamduan.mfu.ac.th>
2024-06-28 16:22:25 +0700

    edit server and ap model

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const wss = new WebSocket.Server({ port: 3003 });
 
 wss.on('connection', function connection(ws) { // สร้าง connection
   console.log("Aruba Websocket Established");
-  ws.on('message',function incoming(message) {
+  ws.on('message', function incoming(message) {
     let telemetryReport = aruba_telemetry_proto.Telemetry.decode(message);
     let myobj = JSON.stringify(telemetryReport);
     let obj = JSON.parse(myobj);
@@ -34,7 +34,6 @@ wss.on('connection', function connection(ws) { // สร้าง connection
 });
 
 async function add_sensors(location, sensor) {
-  await add_ap(location);
   let count = 0;
   for (k of sensor) {
     if (sensor[count]["deviceClass"].includes('iBeacon') == true && sensor[count]["deviceClass"].includes('arubaBeacon') == false && sensor[count]['rssi'] != null) {
@@ -45,62 +44,58 @@ async function add_sensors(location, sensor) {
         timeStamp: new Date().toISOString(),
         major: sensor[count]["beacons"][0]['ibeacon']['major'],
         minor: sensor[count]["beacons"][0]['ibeacon']['minor'],
-        apName: location
+        location: location
       };
-      await add_db(data);
+      await add_db(location, data);
     } else if (sensor[count]["deviceClass"] == 'arubaTag' && sensor[count]['rssi'] != null) {
       let data = {
         tagMac: sensor[count]['mac'],
         deviceClass: 'arubaTag',
         rssi: sensor[count]['rssi']['history'],
         timeStamp: new Date().toISOString(),
-        apName: location,
+        location: location,
         battery: sensor[count]['sensors']['battery']
       };
-      await add_db(data);
+      await add_db(location, data);
     } else if (sensor[count]["deviceClass"] == 'eddystone' && sensor[count]['rssi'] != null) {
       let data = {
         tagMac: sensor[count]['mac'],
         deviceClass: 'eddystone',
         rssi: sensor[count]['rssi']['history'],
         timeStamp: new Date().toISOString(),
-        apName: location,
+        location: location,
         dynamicValue: sensor[count]['sensors']['temperatureC']
       };
-      await add_db(data);
+      await add_db(location, data);
     } else if (sensor[count]["deviceClass"] == 'unclassified' && sensor[count]['rssi'] != null) {
       let data = {
         tagMac: sensor[count]['mac'],
         deviceClass: 'unclassified',
         rssi: sensor[count]['rssi']['history'],
         timeStamp: new Date().toISOString(),
-        apName: location,
+        location: location,
         dynamicValue: sensor[count]['stats']['frame_cnt']
       };
-      await add_db(data);
+      await add_db(location, data);
     }
     count += 1;
   }
 }
 
-async function add_ap(apName){
-  // Check if location has already been processed
-  if (!seenLocations.has(apName)) {
-    var doc = await AccessPoint.findOneAndUpdate(
-      { apName: apName },
-      { $setOnInsert: { apName: apName } },
-      { upsert: true, new: true }
-    );
-    if(doc){
-      seenLocations.add(apName); // Mark this location as seen
-    }
-  }
-}
-
-async function add_db(data) {
+async function add_db(location, data) {
   const tagMac = data.tagMac;
   const deviceClass = data.deviceClass;
   const battery = data.battery || "-";
+
+  // Check if location has already been processed
+  if (!seenLocations.has(location)) {
+    await AccessPoint.findOneAndUpdate(
+      { location: location },
+      { $setOnInsert: { location: location } },
+      { upsert: true, new: true }
+    );
+    seenLocations.add(location); // Mark this location as seen
+  }
 
   // Always update the battery and deviceClass for the tag
   await Tags.findOneAndUpdate(
