748181ab12b43f53626e429d83deaa7feecc31fc
bank-github <6431501089@lamduan.mfu.ac.th>
2024-06-28 11:32:39 +0700

    simplify server.js

--- server.js
+++ server.js
@@ -1,62 +1,41 @@
-var mysql = require('mysql');
 const WebSocket = require('ws');
-const protobuf = require('protobufjs');
 const aruba_telemetry_proto = require('./aruba_iot_proto.js').aruba_telemetry;
 const connectDB = require('./mongoConnect.js');
-const { AccessPoint, Tags, SignalReport } = require('./model.js');
+const { AccessPoint, SignalReport, Tags } = require('./model.js');
 
-var seenLocations;
+// set variable to keep each ap location
+var seenLocations = new Set();
 connectDB();
 
+// สร้าง websockets server ที่ port 3003
 const wss = new WebSocket.Server({ port: 3003 });
 
-// สร้าง websockets server ที่ port 4000
 wss.on('connection', function connection(ws) { // สร้าง connection
   console.log("Aruba Websocket Established");
   ws.on('message', function incoming(message) {
-
-    // รอรับ data อะไรก็ตาม ที่มาจาก client แบบตลอดเวลา
-    //console.log('received: %s', message);
     let telemetryReport = aruba_telemetry_proto.Telemetry.decode(message);
-    //telemetryReport.toJSON();
     let myobj = JSON.stringify(telemetryReport);
     let obj = JSON.parse(myobj);
-    //console.log(myobj);
 
     if (obj["reported"] == null) {
-      console.log("AP not reported ");
+      console.log("AP send message but not have reported");
     } else {
-      // console.log(myobj);
       console.log(obj["reporter"]["name"] + ": " + obj.reported.length);
-      //console.log(obj["reporter"]["ipv4"]);
-      // console.log(obj["reported"]);
       console.log(obj.reported);
       add_sensors(obj["reporter"]["name"], obj.reported);
     }
-    //console.log(telemetryReport.body);
   });
+
   ws.on('close', function close() {
-    // จะทำงานเมื่อปิด Connection ในตัวอย่างคือ ปิด Browser
     console.log('disconnected');
   });
+
   ws.send('init message to client');
-  // ส่ง data ไปที่ client เชื่อมกับ websocket server นี้
 });
-// async function beacon(report) {
-//   var c = 0;
-//   for (k of report) {
-//     c += 1;
-//     console.log(c);
-//     console.log(k.beacons);
-//   }
-// }
 
 async function add_sensors(location, sensor) {
-  // await beacon(sensor);
-  seenLocations = new Set();
   let count = 0;
   for (k of sensor) {
-    // console.log(seenLocations);
     if (sensor[count]["deviceClass"].includes('iBeacon') == true && sensor[count]["deviceClass"].includes('arubaBeacon') == false && sensor[count]['rssi'] != null) {
       let data = {
         tagMac: sensor[count]['mac'],
@@ -68,7 +47,6 @@ async function add_sensors(location, sensor) {
         location: location
       };
       await add_db(data);
-
     } else if (sensor[count]["deviceClass"] == 'arubaTag' && sensor[count]['rssi'] != null) {
       let data = {
         tagMac: sensor[count]['mac'],
@@ -102,26 +80,9 @@ async function add_sensors(location, sensor) {
     }
     count += 1;
   }
-  seenLocations.clear();
 }
 
-// const data = require('./BLE.SignalReport.json');
-// getdata(data);
-// async function getdata(data){
-//  for(dt of data){
-//   const newData = {
-//     tagMac: dt.mac,
-//     diviceClass: dt.deviceClass,
-//     rssi: dt.rssi,
-//     timeStamp: dt.timeStamp,
-//     location: dt.location
-//   }
-//   await add_db(newData);
-//  }
-// }
-
 async function add_db(data) {
-  // console.log("add: "+ data.tagMac)
   const location = data.location;
   const tagMac = data.tagMac;
   const deviceClass = data.deviceClass;
@@ -129,7 +90,6 @@ async function add_db(data) {
 
   // Check if location has already been processed
   if (!seenLocations.has(location)) {
-    // console.log("update AP: "+location)
     await AccessPoint.findOneAndUpdate(
       { location: location },
       { $setOnInsert: { location: location } },
@@ -137,21 +97,17 @@ async function add_db(data) {
     );
     seenLocations.add(location); // Mark this location as seen
   }
-  if (!seenLocations.has(tagMac)) {
-    console.log("update Tag: " + tagMac)
-    await Tags.findOneAndUpdate(
-      { tagMac: tagMac },
-      {
-        $setOnInsert: { tagMac: tagMac, deviceClass: deviceClass },
-        $set: { battery: battery }
-      },
-      { upsert: true, new: true }
-    );
-    seenLocations.add(tagMac); // Mark this location as seen
-  }
+
+  // Always update the battery and deviceClass for the tag
+  await Tags.findOneAndUpdate(
+    { tagMac: tagMac },
+    {
+      $set: { battery: battery, deviceClass: deviceClass }
+    },
+    { upsert: false, new: true } //if not found tag not add to db
+  );
 
   // Insert the signal report
   const newSignalReport = new SignalReport(data);
   await newSignalReport.save();
 }
-
