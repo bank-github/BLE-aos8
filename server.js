4b04ae7bf3b3a4dc0430b11f3f65de4b60261a24
bank-github <6431501089@lamduan.mfu.ac.th>
2024-06-28 15:59:44 +0700

    edit server and ap model

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const wss = new WebSocket.Server({ port: 3003 });
 
 wss.on('connection', function connection(ws) { // สร้าง connection
   console.log("Aruba Websocket Established");
-  ws.on('message',async function incoming(message) {
+  ws.on('message',function incoming(message) {
     let telemetryReport = aruba_telemetry_proto.Telemetry.decode(message);
     let myobj = JSON.stringify(telemetryReport);
     let obj = JSON.parse(myobj);
@@ -21,7 +21,6 @@ wss.on('connection', function connection(ws) { // สร้าง connection
       console.log("AP send message but not have reported");
     } else {
       console.log(obj["reporter"]["name"] + ": " + obj.reported.length);
-      await add_ap(obj["reporter"]["name"]);
       console.log(obj.reported);
       add_sensors(obj["reporter"]["name"], obj.reported);
     }
@@ -34,19 +33,8 @@ wss.on('connection', function connection(ws) { // สร้าง connection
   ws.send('init message to client');
 });
 
-async function add_ap(apName){
-  // Check if location has already been processed
-  if (!seenLocations.has(apName)) {
-    await AccessPoint.findOneAndUpdate(
-      { apName: apName },
-      { $setOnInsert: { apName: apName } },
-      { upsert: true, new: true }
-    );
-    seenLocations.add(apName); // Mark this location as seen
-  }
-}
-
 async function add_sensors(location, sensor) {
+  await add_ap(location);
   let count = 0;
   for (k of sensor) {
     if (sensor[count]["deviceClass"].includes('iBeacon') == true && sensor[count]["deviceClass"].includes('arubaBeacon') == false && sensor[count]['rssi'] != null) {
@@ -92,6 +80,18 @@ async function add_sensors(location, sensor) {
       await add_db(data);
     }
     count += 1;
+  }
+}
+
+async function add_ap(apName){
+  // Check if location has already been processed
+  if (!seenLocations.has(apName)) {
+    await AccessPoint.findOneAndUpdate(
+      { apName: apName },
+      { $setOnInsert: { apName: apName } },
+      { upsert: true, new: true }
+    );
+    seenLocations.add(apName); // Mark this location as seen
   }
 }
 
